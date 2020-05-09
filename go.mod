f20588db8f21eb0ba3a39f952051abb56d329d12
Kamil Samigullin <kamil@samigullin.info>
2020-05-09 12:59:55 +0300

    extend template boilerplate

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.11
 require (
 	github.com/spf13/cobra v1.0.0
 	github.com/stretchr/testify v1.5.1
+	go.octolab.org v0.0.26
 	go.octolab.org/toolkit/cli v0.0.9
 )
--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,1 +1,18 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+// New returns the new root command.
+func New() *cobra.Command {
+	command := cobra.Command{
+		Use:   "%template%",
+		Short: "%template%",
+		Long:  "%template%",
+
+		SilenceErrors: false,
+		SilenceUsage:  true,
+	}
+	/* configure instance */
+	command.AddCommand( /* related commands */ )
+	return &command
+}
