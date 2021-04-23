62e7f9eee82d86a673c9a15a8708aabe6f6c99f6
Kamil Samigullin <kamil@samigullin.info>
2021-04-23 16:21:18 +0300

    issue #5: implement panic command

--- internal/cmd/demo/panic.go
+++ internal/cmd/demo/panic.go
@@ -1,17 +1,23 @@
 package demo
 
-import "github.com/spf13/cobra"
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 // Panic returns a demo cobra.Command to raise a panic.
 //
-// 	$ go run main.go panic
+// 	$ go run main.go panic [message]
 //
 func Panic() *cobra.Command {
 	command := cobra.Command{
-		Use:  "panic",
-		Args: cobra.NoArgs,
+		Use: "panic",
 		Run: func(cmd *cobra.Command, args []string) {
-			panic("unexpected panic")
+			if len(args) == 0 {
+				args = []string{"unexpected panic"}
+			}
+			panic(strings.Join(args, " "))
 		},
 	}
 
